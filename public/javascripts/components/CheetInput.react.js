4feb52694417b45588c16535bbf198553ecf4e28
Jamie Patel <jamie@notespublication.com>
2014-11-20 13:45:34 +0000

    Bundled pusher

--- public/javascripts/components/CheetInput.react.js
+++ public/javascripts/components/CheetInput.react.js
@@ -1,11 +1,27 @@
 var React = require('react');
+var $ = require('jquery');
 
 var CheetInput = React.createClass({
+
+	_onKeyDown: function(event){
+		if(event.keyCode === 13){
+			this.postToServer();
+		}
+	},
+
+	postToServer: function(){
+		var newCheet = {cheet: this.refs.cheetInput.getDOMNode().value.trim()}
+		$.post('/cheets', newCheet).success(function(data){
+			this.refs.cheetInput.getDOMNode().value = ''
+		});
+	},
 
 	render: function() {
 		return (
 			<input
-				placeholder="Write your cheet here" />
+				placeholder="Write your cheet here"
+				onKeyDown={this._onKeyDown}
+				ref="cheetInput" />
 		);
 	}
 });
--- routes/cheets.js
+++ routes/cheets.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var Pusher = require('pusher');
+var secrets = require('../config/secrets');
+
+var pusher = new Pusher({
+	appId: secrets.pusher_id,
+	key: secrets.pusher_key,
+	secret: secrets.pusher_secret
+});
+
 var cheets = [
 	{
 		author: "Jamie Patel",
@@ -10,6 +19,10 @@ var cheets = [
 
 router.get('/', function(req, res) {
   res.json(cheets);
+});
+
+router.post('/', function(req, res){
+	pusher.trigger('cheets', 'new-cheet', {cheet: req.body.cheet})
 });
 
 module.exports = router;
